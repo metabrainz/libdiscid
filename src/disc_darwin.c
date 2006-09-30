e71b617ffdd2f04b0c6b74a503566f9e4dfb6920
Lukáš Lalinský <lalinsky@gmail.com>
2006-09-30 00:02:18 +0000

    Updated FSF address.

--- src/disc_darwin.c
+++ src/disc_darwin.c
@@ -17,7 +17,7 @@
    
    You should have received a copy of the GNU Lesser General Public
    License along with this library; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
      $Id: mb_darwin.cpp,v 1.4 2005/10/28 22:05:27 robert Exp $
 

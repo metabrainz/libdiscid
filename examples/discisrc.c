5ec7a59944c6a8fb41ec8a4f1758ba1997e151ad
Johannes Dewender <github@JonnyJD.net>
2013-02-23 01:32:32 +0100

    use feature tests in the discisrc example
    For use in C the discid_has_feature() function is recommended,
    together with the values in the discid_feature enum.

--- examples/discisrc.c
+++ examples/discisrc.c
@@ -26,26 +26,41 @@
 
 
 int main(int argc, char *argv[]) {
-	DiscId *disc = discid_new();
 	int i;
-  char *device = NULL;
+	char *device = NULL;
+	DiscId *disc = discid_new();
 
-  /* If we have an argument, use it as the device name */
-  if (argc > 1)
-      device = argv[1];
+	/* If we have an argument, use it as the device name */
+	if (argc > 1) {
+		device = argv[1];
+	}
+
+	if (!discid_has_feature(DISCID_FEATURE_READ)) {
+		fprintf(stderr, "Error: not implemented on platform\n");
+		return 1;
+	}
 
 	/* read the disc in the default disc drive */
-	if ( discid_read(disc, device) == 0 ) {
+	if (discid_read(disc, device) == 0) {
 		fprintf(stderr, "Error: %s\n", discid_get_error_msg(disc));
 		return 1;
 	}
 
-  printf("MCN      : %s\n", discid_get_mcn(disc));
+	if (discid_has_feature(DISCID_FEATURE_MCN)) {
+		printf("MCN      : %s\n", discid_get_mcn(disc));
+	} else {
+		printf("MCN      : (not implemented)\n");
+	}
 
-	for ( i = discid_get_first_track_num(disc);
-			i <= discid_get_last_track_num(disc); i++ ) {
+	if (discid_has_feature(DISCID_FEATURE_ISRC)) {
+		for ( i = discid_get_first_track_num(disc);
+				i <= discid_get_last_track_num(disc); i++ ) {
 
-		printf("Track %-2d : %s\n", i, discid_get_track_isrc(disc, i));
+			printf("Track %-2d : %s\n", i,
+					discid_get_track_isrc(disc, i));
+		}
+	} else {
+		printf("ISRCs    : (not implemented)\n");
 	}
 
 	discid_free(disc);

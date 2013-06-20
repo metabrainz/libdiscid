7d4372cadad2b2855aad5ea8f69e8a76393db5c2
Johannes Dewender <github@JonnyJD.net>
2013-06-21 01:58:52 +0200

    some stylistic fixes for discid
    discid_new() is important so it shouldn't be hidden in the declarations.
    The other things are just style/names.

--- examples/discid.c
+++ examples/discid.c
@@ -2,6 +2,7 @@
 
    MusicBrainz -- The Internet music metadatabase
 
+   Copyright (C) 2013 Johannes Dewender, Laurent Monin
    Copyright (C) 2006 Matthias Friedrich
    
    This library is free software; you can redistribute it and/or
@@ -27,6 +28,7 @@
 #endif
 
 #define SECTORS_PER_SECOND 75
+#define ROUND_SECONDS 0
 
 /* Convert a number of sectors to a human-readable time in hours,minutes,seconds
  * If round is true, seconds will be rounded to nearest,
@@ -42,42 +44,47 @@
  */
 void sectors_to_time(int sectors, int round, char *buf, size_t bufsize) {
 	float duration_in_secs = (float) sectors / SECTORS_PER_SECOND;
-	float subsecond = duration_in_secs - (int) duration_in_secs;
 	int hours = duration_in_secs / 3600;
 	int minutes = (duration_in_secs - hours * 3600) / 60;
 	float seconds = duration_in_secs - (hours * 3600 + minutes * 60);
 
 	if (round) {
-		int seconds_rounded = seconds;
-
-		if (subsecond >= 0.5) seconds_rounded++;
-		if (hours > 0)
-			snprintf(buf, bufsize, "%2d:%02d:%02d", hours, minutes, seconds_rounded);
-		else
-			snprintf(buf, bufsize, "%2d:%02d", minutes, seconds_rounded);
-	}
-	else {
-		if (hours > 0)
-			snprintf(buf, bufsize, "%2d:%02d:%05.2f", hours, minutes, seconds);
-		else
-			snprintf(buf, bufsize, "%2d:%05.2f", minutes, seconds);
+		int seconds_rounded = seconds + 0.5;
+		if (hours > 0) {
+			snprintf(buf, bufsize, "%2d:%02d:%02d",
+				 hours, minutes, seconds_rounded);
+		} else {
+			snprintf(buf, bufsize, "%2d:%02d",
+				 minutes, seconds_rounded);
+		}
+	} else {
+		if (hours > 0) {
+			snprintf(buf, bufsize, "%2d:%02d:%05.2f",
+				 hours, minutes, seconds);
+		} else {
+			snprintf(buf, bufsize, "%2d:%05.2f",
+				 minutes, seconds);
+		}
 	}
 }
 
 int main(int argc, char *argv[]) {
-	DiscId *disc = discid_new();
 	int i, first_track, last_track;
 	char *device = NULL;
-	char time[14];
+	char time_str[14];
 	int sectors;
-	int rounded_seconds = 0;
+	DiscId *disc;
+
+	disc = discid_new();
 
 	/* If we have an argument, use it as the device name */
 	if (argc > 1) {
 		device = argv[1];
+	} else {
+		/* this will use discid_get_default_device() internally */
+		device = NULL;
 	}
 
-	/* read the disc in the specified (or default) disc drive */
 	if (discid_read_sparse(disc, device, 0) == 0) {
 		fprintf(stderr, "Error: %s\n", discid_get_error_msg(disc));
 		return 1;
@@ -92,16 +99,16 @@ int main(int argc, char *argv[]) {
 	printf("Last track    : %d\n", last_track);
 
 	sectors = discid_get_sectors(disc);
-	sectors_to_time(sectors, rounded_seconds, time, sizeof(time));
-	printf("Length        : %d sectors (%s)\n", sectors, time);
+	sectors_to_time(sectors, ROUND_SECONDS, time_str, sizeof time_str);
+	printf("Length        : %d sectors (%s)\n", sectors, time_str);
 
 	for ( i = first_track; i <= last_track; i++ ) {
 		sectors = discid_get_track_length(disc, i);
-		sectors_to_time(sectors, rounded_seconds, time, sizeof(time));
-
+		sectors_to_time(sectors, ROUND_SECONDS,
+				time_str, sizeof time_str);
 		printf("Track %-2d      : %8d %8d (%s)\n",
 				i, discid_get_track_offset(disc, i),
-				sectors, time);
+				sectors, time_str);
 	}
 
 	printf("Submit via    : %s\n", discid_get_submission_url(disc));

77d37612bd10e67543ec0d3342e055a45aeef349
Johannes Dewender <github@JonnyJD.net>
2013-06-21 21:58:59 +0200

    LIB-26: add a simple test with a disc
    This basically replicates the testing already done in python-discid.
    
    I implemented several helper functions. That should pay off when adding
    more tests and using these in the build test suite.

---
+++ test/test_disc.c
@@ -0,0 +1,138 @@
+/* --------------------------------------------------------------------------
+
+   MusicBrainz -- The Internet music metadatabase
+
+   Copyright (C) 2013 Johannes Dewender
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+--------------------------------------------------------------------------- */
+#include <stdio.h>
+#include <string.h>
+
+#include <discid/discid.h>
+
+char details[1024] = "\0";
+int passed = 0;
+int tests = 0;
+
+static void announce(const char *text) {
+	printf("Testing %s ... ", text);
+}
+
+static void evaluate(int result) {
+	if (result) {
+		printf("OK\n");
+		passed++;
+	} else {
+		printf("Failed\n");
+		if (strlen(details)) {
+			printf(details);
+		}
+	}
+	tests++;
+}
+
+static int assert_true(int result, const char *msg) {
+	if (result) {
+		return 1;
+	} else {
+		snprintf(details, sizeof details, "    %s!\n", msg);
+		return 0;
+	}
+}
+
+static int equal_int(int result, int expected, const char *msg) {
+	if (expected == result) {
+		return 1;
+	} else {
+		snprintf(details, sizeof details,
+				"    %s:\n\tExpected : %d\n\tActual   : %d\n",
+				msg, expected, result);
+		return 0;
+	}
+}
+
+
+static int test_read_simple() {
+	DiscId *d;
+	int i, first, last;
+	int offset, previous_offset;
+
+	d = discid_new();
+	discid_read_sparse(d, NULL, 0);
+
+	if (!equal_int(strlen(discid_get_id(d)), 28, "Invalid Disc ID")) {
+		discid_free(d); return 0;
+	}
+	if (!equal_int(strlen(discid_get_freedb_id(d)), 8,
+				"Invalid FreeDB ID")) {
+		discid_free(d); return 0;
+	}
+	if (!assert_true(strlen(discid_get_submission_url(d)) > 0,
+				"No submission url")) {
+		discid_free(d); return 0;
+	}
+	first = discid_get_first_track_num(d);
+	last = discid_get_last_track_num(d);
+	if (!equal_int(discid_get_sectors(d),
+				discid_get_track_offset(d, last)
+				+ discid_get_track_length(d, last),
+				"Disc length mismatch")) {
+		discid_free(d); return 0;
+	}
+	previous_offset = 0;
+	for (i=first; i<=last; i++) {
+		offset = discid_get_track_offset(d, i);
+		if (!assert_true(offset <= discid_get_sectors(d),
+					"Invalid offset")) {
+			discid_free(d); return 0;
+		}
+		if (previous_offset) {
+			if (!assert_true(offset >= previous_offset,
+						"Invalid offset series")) {
+				discid_free(d); return 0;
+			}
+		}
+		previous_offset = offset;
+	}
+
+	/* additional features unset */
+	if (!assert_true(strlen(discid_get_mcn(d)) == 0, "MCN not unset")) {
+		discid_free(d); return 0;
+	}
+	for (i=first; i<=last; i++) {
+		if (!assert_true(strlen(discid_get_track_isrc(d, i)) == 0,
+					"ISRC not unset")) {
+			discid_free(d); return 0;
+		}
+	}
+
+	discid_free(d);
+	return 1;
+}
+
+int main(int argc, char *argv[]) {
+
+	announce("simple read");
+	evaluate(test_read_simple());
+
+	printf("\n%d tests, %d passed, %d failed\n",
+	       tests, passed, tests - passed);
+	
+	return passed != tests;
+}
+
+/* EOF */

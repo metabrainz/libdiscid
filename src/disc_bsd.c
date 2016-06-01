a1f7d679ace0fbb7710abb74fe50d4608921603a
Tim Van Holder <tim.vanholder@gmail.com>
2016-06-01 02:12:52 +0200

    Removed trailing whitespace.

--- src/disc_bsd.c
+++ src/disc_bsd.c
@@ -2,21 +2,21 @@
 
    MusicBrainz -- The Internet music metadatabase
 
-   Copyright (C) 2008 Patrick Hurrelmann 
+   Copyright (C) 2008 Patrick Hurrelmann
    Copyright (C) 2006 Matthias Friedrich
    Copyright (C) 2000 Robert Kaye
    Copyright (C) 1999 Marc E E van Woerkom
-   
+
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
    version 2.1 of the License, or (at your option) any later version.
-   
+
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Lesser General Public License for more details.
-   
+
    You should have received a copy of the GNU Lesser General Public
    License along with this library; if not, write to the Free Software
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -126,7 +126,7 @@ void mb_disc_unix_read_mcn(int fd, mb_disc_private *disc) {
 				 MCN_STR_LENGTH );
 		else
 			strncpy( disc->mcn, "*invalid*", MCN_STR_LENGTH );
-	}  
+	}
 }
 
 void mb_disc_unix_read_isrc(int fd, mb_disc_private *disc, int track_num) {
@@ -150,7 +150,7 @@ void mb_disc_unix_read_isrc(int fd, mb_disc_private *disc, int track_num) {
 				 ISRC_STR_LENGTH );
 		else
 			strncpy( disc->isrc[track_num], "*invalid*", ISRC_STR_LENGTH );
-	}  
+	}
 }
 
 int mb_disc_has_feature_unportable(enum discid_feature feature) {
